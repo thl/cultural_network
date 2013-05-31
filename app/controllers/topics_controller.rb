aea10ef79e6cfd64bfa4d6d57f1e7beddebef210
Andres Montano <amontano@virginia.edu>
2013-05-31 07:51:55 -0600

    fixing link

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -11,7 +11,7 @@ class TopicsController < ApplicationController
       set_common_variables(session)
       @object_type = Topic.human_name(:count => :many).titleize
       @object_title = @category.title
-      @object_url = Category.element_url(@category.id, :format => 'html')
+      @object_url = SubjectsIntegration::SubjectsResource.get_url + "features/#{@category.id}"
       @features = Feature.where('cumulative_category_feature_associations.category_id' => @category.id, 'cached_feature_names.view_id' => current_view.id).joins(:cumulative_category_feature_associations).includes(:cached_feature_names => :feature_name).paginate(:page => params[:page] || 1, :per_page => 15).order('feature_names.name')
       @feature = Feature.find(session[:interface][:context_id]) unless session[:interface][:context_id].blank?
       respond_to do |format|
