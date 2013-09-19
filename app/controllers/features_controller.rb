83a55f50a73e24774d7149ef5e400ee65ee51b41
Andres Montano <amontano@virginia.edu>
2013-09-19 14:53:27 -0600

    getting feature_list out of kmaps since there are issues with overriding it in places

--- app/controllers/features_controller.rb
+++ app/controllers/features_controller.rb
@@ -212,21 +212,6 @@ class FeaturesController < ApplicationController
     end
   end
   
-  def related_list
-    @feature = Feature.find(params[:id])
-    @feature_relation_type = FeatureRelationType.find(params[:feature_relation_type_id])
-    @feature_is_parent = params[:feature_is_parent].to_i
-    if @feature_is_parent==0
-      @relations = FeatureRelation.where(:feature_relation_type_id => @feature_relation_type.id, :child_node_id => @feature.id, 'cached_feature_names.view_id' => current_view.id).joins(:parent_node => {:cached_feature_names => :feature_name}).order('feature_names.name')
-    else
-      @relations = FeatureRelation.where(:feature_relation_type_id => @feature_relation_type.id, :parent_node_id => @feature.id, 'cached_feature_names.view_id' => current_view.id).joins(:child_node => {:cached_feature_names => :feature_name}).order('feature_names.name')
-    end
-    @total_relations_count = @relations.length
-    @relations = @relations.paginate(:page => params[:page] || 1, :per_page => 8)
-    @params = params
-    # render related_list.js.erb
-  end
-  
   def children
     feature = Feature.get_by_fid(params[:id])
     @features = feature.children
