66f10819253e2a4217deaf0dbfb558fe81048ba9
Andres Montano <amontano@virginia.edu>
2013-10-23 14:43:24 -0600

    using roman and scholar css for correct rendering of xml essays

--- lib/kmaps_engine/engine.rb
+++ lib/kmaps_engine/engine.rb
@@ -3,7 +3,8 @@ module KmapsEngine
     initializer :assets do |config|
       Rails.application.config.assets.precompile.concat(['kmaps_engine/admin.js', 'kmaps_engine/treescroll.js',
         'kmaps_engine/top.js', 'kmaps_engine/iframe.js', 'kmaps_engine/jquery.ajax.sortable.js',
-        'kmaps_engine/admin.css', 'kmaps_engine/public.css', 'kmaps_engine/xml-books.css'])
+        'kmaps_engine/admin.css', 'kmaps_engine/public.css', 'kmaps_engine/xml-books.css',
+        'kmaps_engine/scholar.css', 'kmaps_engine/popular.css'])
     end
     
     initializer :sweepers do |config|
